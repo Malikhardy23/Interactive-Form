07dce0562571c0a485d1f498647eacc248fdca2d
Malikhardy23 <50418576+Malikhardy23@users.noreply.github.com>
2020-10-05 10:16:38 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const name = document.getElementById("name");
 const otherTitle = document.getElementById("other-title");
 const otherCheckBox = document.getElementById("title");
+const email = document.getElementById("mail");
 
 
 // NAME FOCUS //
@@ -107,21 +108,16 @@ activitiesError.innerHTML = 'Please Select an Option'
 // CHECKBOX VARIABLES //
 
 const activities = document.querySelector('.activities');
-const checkboxes = document.querySelectorAll('label input');
 
 const mainConference = document.getElementsByName("all");
-
-let disabled = () => {
-    document.getElementsByName("express").disabled = true;
-}
 
 let totalElement = document.createElement('h4');
 
 let totalDiv = document.createElement("div");
 
-let totalCost = 0;
-
 activities.appendChild(totalDiv);
+
+totalDiv.appendChild(totalElement);
 
 let total = 0;
 
@@ -129,25 +125,151 @@ let total = 0;
 
 activities.addEventListener('change', (e) => {
 // CHECK THE VALUE OF CHECKBOX //
-    let checked = e.target;
+    let check = e.target;
     let checkboxActivities = document.querySelectorAll('input[type="checkbox"]');
-    let date = checked.getAttribute("data-day-and-time");
-    let cost = checked.getAttribute("data-cost");
+    let date = check.getAttribute("data-day-and-time");
+    let cost = check.getAttribute("data-cost");
     cost = parseInt(cost);
-   if(e.target === checked){
-        totalCost = totalCost + cost;
-        console.log(totalCost);
+
+    if(check.checked === true){
+        total = total + cost
+    } else if(check.checked === false){
+        total = total - cost
     }
-    else {
-        console.log("false");
-    }
 
+    totalElement.textContent = `Total $${total}`;
 
-    // DISABLE CHECK BOX //
+    for(let i = 0; i < checkboxActivities.length; i++){
+        checkboxActivities[i];
+        const input = checkboxActivities[i].getAttribute("data-day-and-time");
 
-    if(e.target.name === "express"){
-        document.getElementsByName("js-frameworks").disabled = true;
+        // DISABLING INPUTS //
+
+        if(date === input && check !== checkboxActivities[i]){
+            if(checkboxActivities[i].disabled){
+                checkboxActivities[i].disabled = false
+            }
+            else {
+                checkboxActivities[i].disabled = true
+            }
+        }
+
     }
 })
 
 
+
+// PAYMENT SECTION //
+
+let paymentOption = document.getElementById("payment");
+
+let creditCard = document.getElementById("credit-card");
+
+let selectMethod = document.getElementsByTagName("option")[20];
+
+
+// HIDE PAYMENT MESSAGE ON CLICK //
+
+selectMethod.hidden = true;
+
+// PAYMENT ADDEVENTLISTENER //
+
+paymentOption.addEventListener('change', (e) => {
+
+
+    if(e.target.value == "bitcoin" || e.target.value == "paypal"){
+        creditCard.style.display = "none";
+    }
+
+    else if(e.target.value == "credit card"){
+        creditCard.style.display = "block";
+    }
+
+})
+
+
+
+// Form Validation //
+
+// NAME VALIDATOR //
+
+const alertValidator = (show, e) => {
+    if(show){
+        e.style.display = "red";
+    } else {
+        e.style.display = "blue";
+    }
+}
+
+const createEventListener = validator => {
+    return e => {
+        let text = e.target.value;
+        let valid = validator(text);
+        let showTip = text !== "" && !valid;
+        let toolTip = e.target.nextElementSibling;
+        alertValidator(showTip, toolTip);
+    };
+}
+
+// VALID NAME VALIDATOR FUNCTION //
+
+const isValidName = e => {
+    e.preventDefault;
+    let name = document.getElementById("name").value;
+    let regexName =  /^[a-zA-Z]+( [a-zA-Z]+)+$/;
+    if(!regexName.test(name)){
+        console.log("Invalid Name");
+        document.getElementById("name").focus();
+    } else {
+        console.log("Valid Name");
+        return true;
+    }
+}
+// CREDIT CARD REGEX //
+
+let visaRegex = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+let masterCardRegex = /^(?:5[1-5][0-9]{14})$/;
+let amexCardRegex = /^(?:3[47][0-9]{13})$/;
+let discCardRegex = /^(?:6(?:011|5[0-9][0-9])[0-9]{12})$/;
+let isValid = false;
+
+// CC VALIDATOR FUNCTION //
+
+const isValidCreditCard = () => {
+    let ccNum = document.getElementById("credit-card").value;
+
+    let isVisa = visaRegex.test(ccNum) === true;
+    let isMasterCard = masterCardRegex.test(ccNum) === true;
+    let isAmex = amexCardRegex.test( ccNum ) === true;
+    let isDisc = discCardRegex.test( ccNum ) === true;
+
+    if(visaRegex || masterCardRegex || amexCardRegex || discCardRegex){
+        console.log("Valid Credit Card");
+    } else {
+        console.log("Invalid Credit Card");
+    }
+    
+}
+
+// EMAIL VALIDATOR FUNCTION //
+
+const isValidEmail = e => {
+    let email = document.getElementById("mail").value;
+    let regexEmail = /^[^@]+@[^@.]+\.[a-z]+$/i;
+    if(!regexEmail.test(email)){
+        console.log("Invalid Name");
+        document.getElementById("mail").focus();
+    } else {
+        console.log("Valid Name");
+        return true;
+    }
+}
+
+
+
+
+name.addEventListener("input", createEventListener(isValidName));
+
+email.addEventListener("input", createEventListener(isValidEmail));
+
+creditCard.addEventListener("input", createEventListener(isValidCreditCard));
